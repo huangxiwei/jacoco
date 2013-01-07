e7161072f7d000bf84469a464af958c0b0b9d719
Martin <mchr3k@gmail.com>
2013-01-07 00:29:45 +0000

    Fix InstrumentTaskTest failure

--- org.jacoco.ant/src/org/jacoco/ant/InstrumentTask.java
+++ org.jacoco.ant/src/org/jacoco/ant/InstrumentTask.java
@@ -85,16 +85,18 @@ public class InstrumentTask extends Task {
 		final File file = new File(destdir, resource.getName());
 		file.getParentFile().mkdirs();
 		try {
-			input = resource.getInputStream();
-			output = new FileOutputStream(file);
-			return instrumenter.instrumentAll(input, output);
+			try {
+				input = resource.getInputStream();
+				output = new FileOutputStream(file);
+				return instrumenter.instrumentAll(input, output);
+			} finally {
+				FileUtils.close(input);
+				FileUtils.close(output);
+			}
 		} catch (final Exception e) {
 			file.delete();
 			throw new BuildException(format("Error while instrumenting %s",
 					resource), e, getLocation());
-		} finally {
-			FileUtils.close(input);
-			FileUtils.close(output);
 		}
 	}
 }
